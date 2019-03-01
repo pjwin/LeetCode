9a77f59ea63c097d54a484804877530c5fd63621
pwinters <pwinters@NJ01-PC0H7B7T.ics.iconectiv.com>
2019-03-01 09:20:21 -0500

    pascal's triangle I and II

---
+++ src/main/java/leetcode/PascalTriangle.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+		PascalTriangle pt = new PascalTriangle();
+//		pt.generate(4);
+//		System.out.println(Arrays.asList(pt.generate(10)));
+		System.out.println(Arrays.asList(pt.getRow(3)));
+	}
+    
+    
+    public List<Integer> getRow(int rowIndex) {
+		if (rowIndex < 0) return null;
+		List<Integer> result = new ArrayList<Integer>();
+		for (int i = 0; i <= rowIndex; i++) {
+			List<Integer> row = new ArrayList<Integer>();
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					row.add(1);
+				} else {
+					row.add(result.get(j) + result.get(j-1));
+				}
+			}
+			result = row;
+		}
+		return result;
+    }
+
+	public List<List<Integer>> generate(int numRows) {
+		if (numRows < 0) return null;
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		for (int i = 0; i < numRows; i++) {
+			List<Integer> row = new ArrayList<Integer>();
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i) {
+					row.add(1);
+				} else {
+					row.add(result.get(i - 1).get(j) + result.get(i - 1).get(j-1));
+				}
+			}
+			result.add(row);
+		}
+		return result;
+    }
+}
